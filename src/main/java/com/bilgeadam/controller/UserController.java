be303f8c095483fbabc0484f1f3312316c897b10
BertanMortas <bertanmortas@gmail.com>
2023-04-04 16:27:33 +0300

    eklemeler

--- src/main/java/com/bilgeadam/controller/UserController.java
+++ src/main/java/com/bilgeadam/controller/UserController.java
@@ -1,96 +1,112 @@
 package com.bilgeadam.controller;
 
-import com.bilgeadam.constant.EndPointList;
 import com.bilgeadam.dto.request.UserRegisterRequestDto;
+import com.bilgeadam.dto.request.UserUpdateRequestDto;
 import com.bilgeadam.dto.response.UserLoginResponseDto;
 import com.bilgeadam.entity.User;
 import com.bilgeadam.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import static com.bilgeadam.constant.EndPointList.*;
 
-import javax.persistence.Enumerated;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(USER)
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    @PostMapping(REGISTER)
+    @PostMapping("/register")
     public ResponseEntity<User> register(String name,String surname,String email,String password,String repassword){
         return ResponseEntity.ok(userService.register(name, surname, email, password, repassword));
     }
 
-    @PostMapping(REGISTER_DTO)
+    @PostMapping("/register-dto")
     public ResponseEntity<UserRegisterRequestDto> registerDto(@RequestBody UserRegisterRequestDto dto){
         return ResponseEntity.ok(userService.registerDto(dto));
     }
 
-    @PostMapping(REGISTER_MAPPER)
+    @PostMapping("/register-mapper")
     public ResponseEntity<UserRegisterRequestDto> registerMapper(@RequestBody @Valid UserRegisterRequestDto dto){
         return ResponseEntity.ok(userService.registerMapper(dto));
     }
-
-    @PostMapping(LOGIN)
+    @PostMapping("/login")
     public ResponseEntity<String> login(String email, String password){
         return ResponseEntity.ok(userService.login(email, password));
     }
-    @PostMapping(LOGIN_DTO)
+    @PostMapping("/login-dto")
     public ResponseEntity<String> loginDto(@RequestBody UserLoginResponseDto dto){
         return ResponseEntity.ok(userService.loginDto(dto));
     }
-    @PostMapping(LOGIN_MAPPER)
+    @PostMapping("/login-mapper")
     public ResponseEntity<UserLoginResponseDto> loginMapper(@RequestBody UserLoginResponseDto dto){
         return ResponseEntity.ok(userService.loginMapper(dto));
     }
-    @PostMapping(CUSTOM_LOGIN)
+
+    @PostMapping("/custom-login")
     public ResponseEntity customLogin(@RequestBody UserLoginResponseDto dto){
-        return userService.costomLogin(dto);
+        return userService.customLogin(dto);
     }
-    @GetMapping(FIND_ALL)
+
+    @GetMapping("/find-all")
     public ResponseEntity<List<User>> findAll(){
         return ResponseEntity.ok(userService.findAll());
     }
 
-    @GetMapping(FIND_BY_ID)
+    @GetMapping("/find-by-id")
     public ResponseEntity<Optional<User>> findById(Integer id){
         return ResponseEntity.ok(userService.findById(id));
     }
-    @DeleteMapping(DELETE)
-    public ResponseEntity<User> delete(Integer id){
+
+    @PutMapping("/update-dto")
+    public ResponseEntity<User> updateDto(UserUpdateRequestDto dto){
+        return ResponseEntity.ok(userService.updateDto(dto));
+    }
+
+    @PutMapping("/update-mapper")
+    public ResponseEntity<User> updateMapper(UserUpdateRequestDto dto){
+        return ResponseEntity.ok(userService.updateMapper(dto));
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<User> delete(@PathVariable Integer id){
         return ResponseEntity.ok(userService.delete(id));
     }
 
-    @GetMapping(FIND_BY_ORDER_NAME_ASC)
+    @GetMapping("/find-by-order-name-asc")
     public ResponseEntity<List<User>> findByOrderByName(){
         return ResponseEntity.ok(userService.findByOrderByName());
     }
 
-    @GetMapping(FIND_BY_NAME_CONTAINING)
+    @GetMapping("/find-by-name-contains")
     public ResponseEntity<List<User>> findAllByNameContainsIgnoreCase(String value){
         return ResponseEntity.ok(userService.findAllByNameContainsIgnoreCase(value));
     }
 
-    @GetMapping(EXISTS_NAME)
+    @GetMapping("/exist-name")
     public ResponseEntity<Boolean> existsByNameIgnoreCase(String value){
         return ResponseEntity.ok(userService.existsByNameIgnoreCase(value));
     }
 
-    @GetMapping(FIND_BY_EMAIL)
+    @GetMapping("/find-by-email")
     public ResponseEntity<List<User>> findByEmailIgnoreCase(String email){
         return ResponseEntity.ok(userService.findByEmailIgnoreCase(email));
     }
-    @GetMapping(FIND_PASSWORD_LENGTH)
-    public ResponseEntity<List<User>> findPasswordLength(int lenght){
-        return ResponseEntity.ok(userService.findPasswordByLength(lenght));
+
+    @GetMapping("/pass-longer-than")
+    public ResponseEntity<List<User>> passwordLongerThan(int num){
+        return ResponseEntity.ok(userService.passwordLongerThan(num));
     }
-    @GetMapping(FIND_PASSWORD_LENGTH_NATIVE)
-    public ResponseEntity<List<User>> findPasswordLength2(int lenght){
-        return ResponseEntity.ok(userService.findPasswordByLengthNative(lenght));
+
+    @GetMapping("/pass-longer-than2")
+    public ResponseEntity<List<User>> passwordLongerThan2(int num){
+        return ResponseEntity.ok(userService.passwordLongerThan2(num));
+    }
+
+    @GetMapping("/find-by-email-ends-with")
+    public ResponseEntity<List<User>> findByEmailEndsWithIgnoreCase(String email){
+        return ResponseEntity.ok(userService.findByEmailEndsWithIgnoreCase(email));
     }
 }
